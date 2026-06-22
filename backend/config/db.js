b8881a128f019262e24e4b6468e93fa1e77a4750
Reva Potdukhe <potdukhereva@gmail.com>
2026-06-23 00:11:06 +0530

    fix mongodb connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,7 @@ const connectDB = async () => {
     console.log("Trying to connect...");
     await mongoose.connect(process.env.MONGO_URI);
     
-    await mongoose.connect(process.env.MONGO_URI);
-
+    
     console.log("MongoDB Connected ✅");
   } catch (error) {
     console.log("DB ERROR:", error.message);
